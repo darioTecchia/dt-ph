39dd56fabfd28807488f063689825eb5ca82a71b
Dario Tecchia <dariotecchia@MacBook-Pro-di-Dario.local>
2020-09-14 18:06:18 +0200

    image lazy load

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="portfolio_single">
     <div class="images-wrapper">
-      <div v-for="(image) in images" :key="image.id" class="image" v-in-viewport>
-        <img :src="image.download_url" alt="" srcset="">
-      </div>
+      <img v-for="(image) in images" :key="image.id" v-in-viewport class="image" :src="image.download_url">
     </div>
   </div>
 </template>
@@ -41,18 +39,7 @@ export default {
   border-radius: 8px;
   padding: 20px 20px 40px 20px;
 
-  -webkit-transition: all 1s var(--f-transition);
-  -moz-transition: all 1s var(--f-transition);
-  -o-transition: all 1s var(--f-transition);
-  transition: all 1s var(--f-transition);
-}
-
-.image.in-viewport {
   box-shadow: 0 13px 27px -5px hsla(240, 30.1%, 28%, 0.25), 0 8px 16px -8px hsla(0, 0%, 0%, 0.3), 0 -6px 16px -6px hsla(0, 0%, 0%, 0.03);
-}
-
-.image img {
-  width: 100%;
 
   -webkit-transition: all 1s var(--f-transition);
   -moz-transition: all 1s var(--f-transition);
@@ -61,11 +48,11 @@ export default {
 }
 
 .lazyLoad {
-  opacity: 0;
+  transform: translateX(-200%);
 }
 
 .isLoaded {
-  opacity: 1;
+  transform: translateX(0%);
 }
 </style>
 
